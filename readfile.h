1cc969461b75adf468841e4caa3fb387f6dbe819
amf5214 <austinfraley.lee@gmail.com>
2024-02-08 23:20:54 -0500

    Added missing documentation to readfile.h
    Added missing documentation to readfile.h file

--- readfile.h
+++ readfile.h
@@ -53,6 +53,16 @@ void printFile(File* file);
 // Returns: int representing the number of words present
 int countWords(char* lineText);
 
+// Function to pop last character from a string/char*
+// Params: string:char* represents the string to pop from, length:int is the length of the string
+// Returns: char* is the pointer to the new string
+char* popCharFromString(char* string, int length);
+
+// Function to remove the next line character from the end of a word
+// Params: word:Word word to check and remove from
+// Returns: Word is the new word object where the \n has been removed 
+Word removeNextLine(Word* word);
+
 // Function to count the number of lines in a file
 // Params: file:FILE* FILE object pointer from std io library storing a stream of data
 // Returns: int representing the number of lines in the file
@@ -76,3 +86,13 @@ void assignNthWord(Line* line, int nthWord);
 // Function to assign the nthword attribute of all Line objects in a File object
 // Params: file:File* file to be updated, nthWord:int index of the nthWord
 void assignNthWords(File* file, int nthWord);
+
+// Function to compare lines of a file
+// Created with the prototype required by qsort function
+// Params: a:const void* is a void pointer to line 1, b:const void* is a void pointer to line 2
+// Returns: int representing whether the nth word is equal to, comes before or after
+int compareLines(const void * a, const void * b);
+
+// Function to free a file object and all of it's components
+// Params: file:File is the file object to be freed
+void freeFile(File file);
